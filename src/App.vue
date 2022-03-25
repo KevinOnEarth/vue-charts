4507c2a58066f0d10ed9c07dfcb7f77d52975efe
Kevin Kapfer <kafeman@hotmail.com>
2022-03-24 20:41:32 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <BarChartComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import BarChartComponent from './components/BarChartComponent.vue';
+
+export default {
+  components: {
+    BarChartComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BarChartComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+    <Plotly :data="data" :layout="getLayout" :display-mode-bar="displayModeBar"></Plotly>
+    </div>
+</template>
+<script lang="ts">
+import {Plotly} from 'vue-plotly'
+import { Layout, TickLocation } from '../models/ChartProperties'
+export default ({
+    name: 'BarChartComponent',
+    components: {
+        Plotly
+    },
+    data: ()=> ({
+        data:[{
+            x: [1,2,3,4],
+            y: [10,15,20,25],
+            type: "bar"
+        },
+        {
+            x: [1,2,3,4],
+            y: [18,28,18,28],
+            type: "line"
+        }],
+        layout: {
+            title: 'Sample Bar Chart'
+        },
+        displayModeBar: false
+    }),
+    computed: {
+        getLayout(): Layout {
+            let layout: Layout = {
+              title: 'Typescript Bar Chart Example',
+              height: 450,
+              showlegend: false,
+              autosize: true,
+              xaxis: {
+                title: { standoff: 2, text:'Something X Axis'},
+                ticks: TickLocation.outside
+              },
+              yaxis: {
+                title: {standoff: 4, text: 'Something Y Axis'},
+                ticks: TickLocation.outside
+              },
+              margin: {
+                l: 40,
+                r: 25,
+                t: 25,
+                b: 40
+              }
+            };
+            return layout
+        }
+    }
+})
+</script>
